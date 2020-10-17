19a693bd8655aa6dfe184366698a55f58aa9786a
rfheise <43056909+rfheise@users.noreply.github.com>
2020-10-17 12:22:59 -0400

    bruh

--- dashit/dining/models.py
+++ dashit/dining/models.py
@@ -1,3 +1,45 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from uuid import uuid4
 # Create your models here.
+class Food(models.Model):
+    name = models.TextField()
+    food_id = models.UUIDField(default=uuid4)
+    rating = models.FloatField()
+
+class Customer(User):
+    phone = models.CharField(max_length=11)
+    user_id = models.UUIDField(default=uuid4)
+
+class Rating(models.Model):
+    rating = models.FloatField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name ="ratings")
+    rating_id = models.UUIDField(default = uuid4)
+
+class DiningHall(models.Model):
+    name = models.TextField()
+    avg_wait = models.FloatField()
+    curr_wait = models.FloatField()
+
+
+class DiningHallRating(Rating):
+    dining_hall = models.OneToOneField(DiningHall,on_delete=models.CASCADE,related_name="ratings")
+
+class FoodRating(Rating):
+    food = models.OneToOneField(Food,on_delete=models.CASCADE,related_name="ratings")
+
+class Comment(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name = "comments")
+    comment = models.TextField()
+    like = models.IntegerField()
+
+class FoodComment(Comment):
+    food = models.OneToOneField(Food,on_delete=models.CASCADE,related_name = "comments")
+
+class DiningHallComment(Comment):
+    dining_hall = models.OneToOneField(DiningHall,on_delete=models.CASCADE,related_name="comments")
+
+class Queue(models.Model):
+    dining_hall = models.ForeignKey(DiningHall,on_delete=models.CASCADE,related_name ="queue")
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="queue")
+    time = models.IntegerField()
