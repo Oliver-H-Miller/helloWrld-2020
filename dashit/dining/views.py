3c752ebdf12d4807800a2e60ffe90e2974aa6d54
sferia003 <67718601+sferia003@users.noreply.github.com>
2020-10-17 19:33:39 -0400

    added images + main

--- dashit/dining/views.py
+++ dashit/dining/views.py
@@ -16,7 +16,7 @@ def dining_hall_view(request,hall):
     # return render(request, hall_template, context)
 
 def index(request):
-    return render(request,"error.html",{"message":""})
+    return render(request,"main.html",{"dining_court":"ChickFilA"})
 
 
 
