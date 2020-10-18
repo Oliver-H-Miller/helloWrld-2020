dada6e48fe85d513767ecbb2218e5dd2192e9cfd
Hunter314 <hunter.haglid@gmail.com>
2020-10-17 20:35:06 -0400

    Added function to retrieve JSON from API and retrieve times from JSON.

---
+++ dashit/dining/helperfunctions/todaysMenus.py
@@ -0,0 +1,53 @@
+import requests
+import json
+from datetime import date
+
+today = date.today()
+weekday = today.weekday()
+# dd/mm/YY
+page = requests.get("https://api.hfs.purdue.edu/menus/v2/locations")
+# print(page.content)
+content = str(page.content)
+jsoncontent = content[2:len(content) - 1]
+jsoncontent = jsoncontent.replace("\\\'","&#39;")
+f = open("menus" + today.strftime("%d-%m-%Y") + ".json", "w")
+f.write(jsoncontent)
+f.close()
+
+def isWeekday(day):
+  if day is weekday:
+    return True
+  else:
+    return False
+
+data = json.loads(jsoncontent)
+for p in data['Location']:
+  print('Name: ' + p['Name'])
+  hours = p['NormalHours']
+  weekTimes = hours[0]['Days']
+  today_times = None
+  #print(weekTimes)
+  # print(weekTimes)
+  print("Today: " + str(weekday))
+  for day in weekTimes:
+    if(day['DayOfWeek'] == weekday):
+      # print("Day time is " + str(day['DayOfWeek']))
+      today_times = day
+  if today_times is None: 
+    continue
+  for meal_time in today_times['Meals']:
+    # Replace meal[Name] with django obj
+    
+    meal_obj = meal_time['Name']
+    hours = meal_time['Hours']
+    start_time = hours['StartTime']
+    end_time = hours['EndTime']
+    print(p['Name'] + " is having " + meal_obj + " from " + start_time + " to " + end_time)
+  
+  ## ALREADY RETRIVED:
+  # TIMES
+  # NAMES
+
+  ## Menu items not found in API.
+
+
