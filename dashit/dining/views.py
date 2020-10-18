0b316301d7fd8f7b4d3e0c52096199cb739c797c
Oliver Miller <mill2731@purdue.edu>
2020-10-18 07:12:10 -0400

    added messages

--- dashit/dining/views.py
+++ dashit/dining/views.py
@@ -20,13 +20,13 @@ def dining_hall_view(request,hall):
 
 def index(request):
     return render(request, 'main.html', {'dining_courts': [
-    {'name': 'JerseyMikes', 'busy': range(5), 'isLive': True, 'timeEstMin': 16},
-    {'name': 'Chick-fil-A', 'busy': range(4), 'isLive': True, 'timeEstMin': 9},
-    {'name': 'Hillenbrand', 'busy': range(2), 'isLive': False, 'timeEstMin': 4},
-    {'name': 'Earhart', 'busy': range(2), 'isLive': False, 'timeEstMin': 4},
-    {'name': 'Wiley', 'busy': range(3), 'isLive': False, 'timeEstMin': 8},
-    {'name': 'Windsor', 'busy': range(1), 'isLive': False, 'timeEstMin': 11},
-    {'name': 'Ford', 'busy': range(1), 'isLive': False, 'timeEstMin': 16},
+    {'name': 'JerseyMikes', 'busy': range(5), 'isLive': True, 'timeEstMin': 16, 'desc': "Great subs but beware the long wait times! To combat this, we've set up live data so you can pick the best time to avoid the line."},
+    {'name': 'Chick-fil-A', 'busy': range(0), 'isLive': False, 'timeEstMin': 0, 'desc': "The best Chicken sandwich, with extreme line efficiency.. except on Sundays."},
+    {'name': 'Hillenbrand', 'busy': range(2), 'isLive': False, 'timeEstMin': 4, 'desc': "A dining hall, nothing remarkable. Very good wait times on average."},
+    {'name': 'Earhart', 'busy': range(2), 'isLive': False, 'timeEstMin': 4, 'desc': "A dining hall, nothing remarkable. Very good wait times on average."},
+    {'name': 'Wiley', 'busy': range(3), 'isLive': False, 'timeEstMin': 8, 'desc': "A dining hall, nothing remarkable. Very good wait times on average."},
+    {'name': 'Windsor', 'busy': range(1), 'isLive': False, 'timeEstMin': 11, 'desc': "A dining hall, nothing remarkable. Very good wait times on average."},
+    {'name': 'Ford', 'busy': range(1), 'isLive': False, 'timeEstMin': 16, 'desc': "A dining hall, nothing remarkable. Very good wait times on average."},
     ]})
 
 
