d91ad4180190a047a7c1d2c8c94fa332b88c555d
Oliver Miller <miller.oliver@icloud.com>
2020-10-17 20:13:13 -0400

    updated helper.py and implemented 'parse' function

--- dashit/dining/helper.py
+++ dashit/dining/helper.py
@@ -1,8 +1,18 @@
 import base64
+import re
 
 def parse(x):
     data = str(base64.b64decode(x))
-    return data
+    p = re.compile(r'(?:[0-9a-fA-F]:?){12}')
+    pattern = re.compile(r'RSSI: (.\d\d)')
+    test_str = u"TEXT WITH SOME MAC ADDRESSES 00:24:17:b1:cc:cc TEXT CONTINUES WITH SOME MORE TEXT 20:89:86:9a:86:24"
+    
+    macAddr = re.findall(p, data)
+    signalStr = re.findall(pattern, data)
+    results = [macAddr, signalStr]
+    return results
+
+
 def main():
     l = ""
     with open("f.txt","r") as f:
